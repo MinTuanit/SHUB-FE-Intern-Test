968ab610863efd414fe85aae292553a4b644d6c2
MinTuan2124 <166462832+MinTuanit@users.noreply.github.com>
2025-08-24 22:43:14 +0700

    Update task2

--- Task2/task2/src/app/page.tsx
+++ Task2/task2/src/app/page.tsx
@@ -2,6 +2,11 @@ import TransactionForm from "../components/transactionForm";
 
 export default function Home() {
   return (
-    <TransactionForm></TransactionForm>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-4xl font-bold text-center">
+        This Project is for Task 2 - SHUB FE Intern Test
+      </h1>
+      <TransactionForm />
+    </div>
   );
 }
